9fb25c79019a09652bf7900b19f4aa913700752a
Raheem Kawojue <kawojue08@gmail.com>
2022-11-11 23:19:43 +0100

    more tailwindcss, colors & fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,22 @@
 module.exports = {
   content: ['./index.html', './src/**/*.jsx'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'poppins': ["Poppins", 'sans-serif']
+      },
+      colors: {
+        'pry-clr-0': 'rgb(241, 245, 248)',
+        'pry-clr-1': 'rgb(6, 50, 81)',
+        'pry-clr-2': 'rgb(97, 125, 152)',
+        'pry-clr-3': 'rgb(104,182,238)',
+        'pry-clr-4': 'rgb(73, 166, 233)',
+        'pry-clr-5': '#e66b6b',
+        'pry-clr-6': '#bb2525',
+        'pry-clr-6': '#6be675',
+        'pry-clr-7': '#25bb32',
+      }
+    },
   },
   plugins: [],
 }
