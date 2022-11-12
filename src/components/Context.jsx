b0ac154d39a7368d9a73607f6514a6a8e1fe30d2
Raheem Kawojue <kawojue08@gmail.com>
2022-11-12 17:21:28 +0100

    json-server & react-icons

--- src/components/Context.jsx
+++ src/components/Context.jsx
@@ -1,11 +1,17 @@
-import { createContext, useEffect, useState } from 'react'
+import { createContext, useEffect, useState, useRef } from 'react'
 
 const Context = createContext({})
 
 export const DataProvider = ({ children }) => {
+    const url = "http://localhost:3500"
+    const inputEl = useRef()
+    const [item, setItem] = useState("")
+    const [items, setItems] = useState([])
+
     return (
         <Context.Provider value={{
-
+            inputEl, item, setItem,
+            items, setItems
         }}>
             {children}
         </Context.Provider>
