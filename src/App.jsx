5a12e0b0782157f9bdc8dbd1e90e2cbd821b8459
Raheem Kawojue <kawojue08@gmail.com>
2022-11-12 18:17:30 +0100

    List Items component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Form from './components/Form'
+import Items from './components/Items'
 
 function App() {
   return (
     <main>
       <h1 className="md:text-3xl">grocery buds</h1>
       <Form />
+      <Items />
     </main>
   )
 }
