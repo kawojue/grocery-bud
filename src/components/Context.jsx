9680e8642fc42c430ba66022b9b42d51580e44db
Raheem Kawojue <kawojue08@gmail.com>
2022-11-12 17:31:55 +0100

    fetched items

--- src/components/Context.jsx
+++ src/components/Context.jsx
@@ -3,10 +3,26 @@ import { createContext, useEffect, useState, useRef } from 'react'
 const Context = createContext({})
 
 export const DataProvider = ({ children }) => {
-    const url = "http://localhost:3500"
+    const url = "http://localhost:3500/items"
     const inputEl = useRef()
     const [item, setItem] = useState("")
     const [items, setItems] = useState([])
+    const [fetchErr, setFetchErr] = useState(null)
+
+    const fetchItems = async () => {
+        try {
+            setFetchErr(null)
+            const res = await fetch(url)
+            const data = await res.json()
+            setItems(data)
+        } catch (err) {
+            setFetchErr("Please, reload the page or check your connection.")
+        }
+    }
+
+    useEffect(() => {
+        (async () => await fetchItems())()
+    }, [])
 
     return (
         <Context.Provider value={{
