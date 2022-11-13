52312196598b462abec69e80d8c33380d256a62c
Raheem Kawojue <kawojue08@gmail.com>
2022-11-13 17:16:43 +0100

    msg shown

--- src/manageState.js
+++ src/manageState.js
@@ -1,14 +1,37 @@
 import {useEffect} from 'react'
 
-const manageState = (list) => {
+const manageState = (command, list) => {
+    const msg = ""
     useEffect(() => {
-        const msg = setInterval(() => {
-            //
+        const value = setInterval(() => {
+            switch(command) {
+                case 'DELETE':
+                    msg = "item deleted"
+                    break
+                case 'ADD':
+                    msg = "item added"
+                    break
+                case 'EDIT':
+                    msg = "value changed"
+                    break
+                default:
+                    msg = ''
+            }
+            // if (command === 'DELETE') {
+            //     msg = "item deleted"
+            // }
+            // if (command === 'EDIT') {
+            //     msg = "value changed"
+            // }
+            // if (command === 'ADD') {
+            //     MSG
+            // }
         }, 1500)
         return () => {
-            clearInterval(msg)
+            clearInterval(value)
         }
     }, [list])
+    return msg
 }
 
 export default manageState
