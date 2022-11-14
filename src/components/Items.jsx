bc091c80071a5cbd31b38d2815a3db20abb8a4f0
Raheem Kawojue <kawojue08@gmail.com>
2022-11-14 16:41:58 +0100

    changed item to name

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -3,15 +3,16 @@ import { useContext } from 'react'
 import { FaTrash, FaEdit } from 'react-icons/fa'
 
 const Items = () => {
-    const { items, deleteItem, clearItems } = useContext(Context)
+    const { items, deleteItem, clearItems,
+        editItem } = useContext(Context)
 
     return (
         <section className="items">
             {items.map(item => (
                 <article key={item.id} className="item">
-                    <p>{item.item}</p>
+                    <p>{item.name}</p>
                     <div className="btn-container">
-                        <button className="edit-btn">
+                        <button className="edit-btn" onClick={() => editItem(item.id)}>
                             <FaEdit />
                         </button>
                         <button className="delete-btn" onClick={() => deleteItem(item.id)}>
