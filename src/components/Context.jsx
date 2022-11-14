b3abeb2fb3dbe40a10de31668aa82f96a471de43
Raheem Kawojue <kawojue08@gmail.com>
2022-11-14 16:32:29 +0100

    add item & edit item

--- src/components/Context.jsx
+++ src/components/Context.jsx
@@ -7,14 +7,16 @@ export const DataProvider = ({ children }) => {
     const url = "http://localhost:3500/items"
 
     const inputEl = useRef()
-    const [item, setItem] = useState("")
+    const [name, setName] = useState("")
     const [items, setItems] = useState([])
     const [msg, setMsg] = useState({})
+    const [ID, setID] = useState(null)
     const [isEditing, setIsEditing] = useState(false)
 
     const fetchItems = async () => {
         try {
             setMsg({})
+            setIsEditing(false)
             const res = await fetch(url)
             const data = await res.json()
             setItems(data)
@@ -23,7 +25,11 @@ export const DataProvider = ({ children }) => {
         }
     }
 
-    const fetchItem = async (id) => {
+    useEffect(() => {
+        (async () => await fetchItems())()
+    }, [])
+
+    const fetchItem = async id => {
         try {
             const res = await fetch(`${url}/${id}`)
             const data = await res.json()
@@ -36,6 +42,8 @@ export const DataProvider = ({ children }) => {
     const deleteItem = async id => {
         const newItems = items.filter(item => item.id !== id)
         setItems(newItems)
+        setIsEditing(false)
+        setName("")
 
         await fetch(`${url}/${id}`, {
             method: 'DELETE'
@@ -43,24 +51,52 @@ export const DataProvider = ({ children }) => {
     }
 
     const clearItems = () => {
+        setIsEditing(false)
+        setName("")
+
         const IDS = items.map(item => item.id)
         IDS.forEach(async id =>
             await fetch(`${url}/${id}`, {
                 method: 'DELETE',
             }))
+
         setItems([])
     }
 
-    useEffect(() => {
-        (async () => await fetchItems())()
-    }, [])
+    const editItem = async id => {
+        setIsEditing(true)
+        const getItem = items.filter(item => item.id === id)
+        setID(getItem[0].id)
+        setName(getItem[0].name)
+    }
+
+    const addItem = async e => {
+        e.preventDefault()
+        const getNewID = new Date().getTime()
+        const getItem = items.filter(item => item.id === ID)
+
+        if (isEditing) {
+            getItem[0].name = name
+            const newItems = items.map(item => item.id === ID ? getItem[0] : item)
+            setItems(newItems)
+            setName("")
+        }
+        const newItem = { id: getNewID, name }
+        setItems([newItem, ...items])
+
+        // Default states
+        setName("")
+        setIsEditing(false)
+        setID(null)
+        inputEl.current.focus()
+    }
 
     return (
         <Context.Provider value={{
-            inputEl, item, setItem,
+            inputEl, name, setName,
             items, setItems, isEditing,
             setIsEditing, msg, deleteItem,
-            clearItems
+            clearItems, editItem, addItem
         }}>
             {children}
         </Context.Provider>
