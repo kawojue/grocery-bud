035064c144f1e3253b6291051ed30c2f7c9f0d5d
Raheem Kawojue <kawojue08@gmail.com>
2022-11-14 16:42:21 +0100

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import { useContext } from 'react'
 import Form from './components/Form'
 import Items from './components/Items'
+import Context from './components/Context'
 
 function App() {
+  // const { msg } = useContext(Context)
+
   return (
     <main>
+      {/* <p className={`bg-${msg.color}`}>{msg.msg}</p> */}
       <h1 className="md:text-3xl">grocery buds</h1>
       <Form />
       <Items />
