97fdb64668f972aed4f709cdedb1d3a7eaa4c264
Raheem Kawojue <kawojue08@gmail.com>
2022-11-15 13:46:04 +0100

    initialized alert

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useContext } from 'react'
 import Form from './components/Form'
 import Items from './components/Items'
 import Context from './components/Context'
+import ManageAlert from './components/ManageAlert'
 
 function App() {
-  // const { msg } = useContext(Context)
+  const { alert, showAlert } = useContext(Context)
 
   return (
     <main>
-      {/* <p className={`bg-${msg.color}`}>{msg.msg}</p> */}
+      {alert.show && <ManageAlert {...alert} remAlert={showAlert} />}
       <h1 className="md:text-3xl">grocery buds</h1>
       <Form />
       <Items />
