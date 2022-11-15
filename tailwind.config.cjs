0cdadcc5d95bc9f29e826c5f36795e46b056ca06
Raheem Kawojue <kawojue08@gmail.com>
2022-11-15 14:42:58 +0100

    added color

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -12,7 +12,7 @@ module.exports = {
         'pry-clr-2': 'rgb(97, 125, 152)',
         'pry-clr-3': 'rgb(104,182,238)',
         'pry-clr-4': 'rgb(73, 166, 233)',
-        // 'pry-clr-5': '#e66b6b',
+        'pry-clr-5': '#ff7f50',
         'pry-clr-6': '#e66b6b',
         'pry-clr-7': '#bb2525',
         'pry-clr-8': '#6be675',
